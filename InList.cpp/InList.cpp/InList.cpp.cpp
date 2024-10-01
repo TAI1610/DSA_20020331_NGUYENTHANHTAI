6192656da92b3cac8505aefee3969a4c6de681bb
TAI1610 <20020331@vnu.edu.vn>
2024-10-01 10:24:56 +0700

    Add files via upload

---
+++ InList.cpp/InList.cpp/InList.cpp.cpp
@@ -0,0 +1,12 @@
+﻿// InList.cpp.cpp : Defines the entry point for the application.
+//
+
+#include "InList.cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ InList.cpp/InList.cpp/InList.cpp.h
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+class IntList {
+private:
+    std::vector<int> data;
+
+public:
+    void addElement(int value) {
+        data.push_back(value);
+    }
+
+    void removeElement(int index) {
+        if (index < 0 || index >= data.size()) {
+            std::cout << "Chỉ số không hợp lệ!\n";
+            return;
+        }
+        data.erase(data.begin() + index);
+    }
+
+    void printList() const {
+        std::cout << "Danh sách: ";
+        for (int value : data) {
+            std::cout << value << " ";
+        }
+        std::cout << "\n";
+    }
+};
+
+int main() {
+    IntList list;
+    list.addElement(10);
+    list.addElement(20);
+    list.addElement(30);
+    list.printList();
+
+    list.removeElement(1);
+    list.printList();
+
+    return 0;
+}
